ab8156bf722f882ed180304099956e74afd0036e
Divyajyoti Rayaguru <divyajyotirayaguru2018@gmail.com>
2026-02-21 18:15:35 +0530

    js fundamnetals

---
+++ Divyajyoti-Rayaguru/javascript/index.js
@@ -0,0 +1,82 @@
+console.log("hello")
+console.log("second")
+
+
+age=22
+console.log(age)
+
+age="hello"
+console.log(age);
+
+
+if(true){
+    var age = 16
+}
+console.log(age);
+
+function getname(){
+    var name="divya"
+}
+console.log(name)
+
+
+if(true){
+    var name="divya"
+}
+console.log(name)
+
+
+//let
+//props: 1)no rdeclaration
+let name1;
+
+// 2)re assign
+// let coll="iter"
+// console.log(coll)
+// coll="iter,bbsr"
+// console.log(coll)
+// coll=56
+// console.log(coll)
+
+
+
+//scoping 1)global scope
+//         2)block scope
+
+
+// const 
+// 1) have to initialise
+// 2) no reassign
+// 3)follows block
+
+
+// const age1=22;
+// console.log(age1)
+
+
+// {
+//     const age2 = 50
+// }
+// console.log(age2)
+
+
+// if(true){
+//     const coll2 = "iter"
+// }
+// console.log(coll2)
+
+
+
+
+
+
+
+// Hoisting
+
+
+
+
+
+{
+    
+}
