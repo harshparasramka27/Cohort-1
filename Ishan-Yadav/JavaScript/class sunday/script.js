67f8933b3d1dc0cd7b77f906dc0385242cd76238
ishanyadav-hash <ishanyadav2707@gmail.com>
2026-02-22 13:08:42 +0530

    Class-2 notes

---
+++ Ishan-Yadav/JavaScript/class sunday/script.js
@@ -0,0 +1,31 @@
+// var x;
+// x=10
+
+// let 
+// let globally and block scope
+// reinitialize but cannot redeclare
+
+// let x=10;
+
+// inside local scope
+// {
+//     let x=11
+// }
+// function name() {
+//     var y=11;
+//     console.log(x)
+//     console.log(y)
+
+// }
+
+//console.log(y)
+
+// name()
+
+// const
+/* global and blocked scope
+redeclare and reinitialized both not possible
+value can't be changed */
+// const x=10
+// x=11 not possible
+
